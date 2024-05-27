9d042969d4b70161234b314a70edcd3639578ab5
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2024-05-27 19:48:19 +0000

    used a function to prevent overwriting instead

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  var task;
-  var task2;
+  var task = false;
+  var task2 = true;
 
-  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-  }();
+  (function() {
+    if (trueOrFalse) {
+      var task = true;
+      var task2 = false;
+    }
+  })();
 
   return [task, task2];
 }

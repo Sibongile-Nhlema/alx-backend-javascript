f4572e1a45ab00092edf9d3ea3c8075cd6916205
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2024-05-27 19:27:51 +0000

    hoisted var so that the variables aren't overwritten

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+export default function taskBlock(trueOrFalse) {
+  var task;
+  var task2;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+  task = false;
+  task2 = true;
+
+  return [task, task2];
+}

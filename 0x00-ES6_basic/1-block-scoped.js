111a5905f457f0983f41ffbfcd345b8b8dcf43ff
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2024-05-27 19:41:12 +0000

    used a paranthesis to call function immediately

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,9 +5,7 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
-  }
-  task = false;
-  task2 = true;
+  }();
 
   return [task, task2];
 }

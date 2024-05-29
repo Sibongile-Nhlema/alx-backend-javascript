7423e21bc479a95f1211183b04492d440e2258d7
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2024-05-29 18:08:21 +0000

    added a return statemnt

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoData, userData]) => {
-      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  return Promise.all([uploadPhoto(), createUser()])
+      .then(([photoData, userData]) => {
+        console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+      })
+      .catch(() => {
+        console.log('Signup system offline');
+      });
 }
 
 export default handleProfileSignup;

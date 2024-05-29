acdb9abd781a55a71452e36e3ef259bae2774bf7
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2024-05-29 16:11:03 +0000

    created three handles to the handleResponseFromAPI function

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((resolve) => ({ status: 200, body: 'Success' }))
+    .catch((error) => ({ error: {} }))
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
+
+export default handleResponseFromAPI;

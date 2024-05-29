7d44d4c238bc9dab41b7360a00b5874033cd81dc
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2024-05-29 16:29:24 +0000

    implemeted the empty error object correctly

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => ({ error: {} }))
+    .catch(() => new Error(''))
     .finally(() => {
       console.log('Got a response from the API');
     });

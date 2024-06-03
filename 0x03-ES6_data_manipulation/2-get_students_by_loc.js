097373185c05e844c9982589d3f96e4f66cec488
Sibongile <sibongile.nhlema@gmail.com>
2024-06-03 19:11:09 +0200

    fixed the eslint errors

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(listOfStudents, city) {
-    // return an array of objects located in a specific city.
-    const filterStudents = listOfStudents.filter(student => student.location === city);
-    return filterStudents;
+  // return an array of objects located in a specific city.
+  const filterStudents = listOfStudents.filter((student) => student.location === city);
+  return filterStudents;
 }

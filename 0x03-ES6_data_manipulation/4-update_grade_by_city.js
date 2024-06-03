6d44a8953e829411649c2cff6e81b8f2aa1ebdaa
Sibongile <sibongile.nhlema@gmail.com>
2024-06-03 19:25:38 +0200

    fixed eslint errors

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,17 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
-    // Filter students based on the specified city
-    const filterStudents = listStudents.filter(student => student.location === city);
+  // Filter students based on the specified city
+  const filterStudents = listStudents.filter((student) => student.location === city);
 
-    // Iterate through filtered students and update their grades if found in newGrades
-    const updatedStudents = filterStudents.map(student => {
-        const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-        if (gradeObj) {
-            student.grade = gradeObj.grade;
-        } else {
-            student.grade = 'N/A';
-        }
-        return student;
-    });
-
-    return updatedStudents;
+  // Iterate through filtered students and update their grades if found in newGrades
+  const updatedStudents = filterStudents.map((student) => {
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+    const updatedStudent = { ...student };
+    if (gradeObj) {
+      updatedStudent.grade = gradeObj.grade;
+    } else {
+      updatedStudent.grade = 'N/A';
+    }
+    return updatedStudent;
+  });
+  return updatedStudents;
 }

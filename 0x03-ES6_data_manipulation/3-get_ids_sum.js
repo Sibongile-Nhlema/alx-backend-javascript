0a071a707a0e7d2955e085b06ecb27d73702d841
Sibongile <sibongile.nhlema@gmail.com>
2024-06-03 18:17:46 +0200

    used reduce to add the sum of all ids

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(listOfStudents) {
+    const initialValue = 0;
+    const callback = (accumulator, currentValue) => accumulator + currentValue.id
+
+    const sumOfStudentIds = listOfStudents.reduce(callback, initialValue)
+    return sumOfStudentIds;
+}

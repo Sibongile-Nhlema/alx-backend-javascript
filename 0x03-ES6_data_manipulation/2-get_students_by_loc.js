344604e828902e731fa2a78888c4c73716042037
Sibongile <sibongile.nhlema@gmail.com>
2024-06-03 17:45:33 +0200

    used filter in order to return the objects that have the same location

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+export default function getStudentsByLocation(listOfStudents, city) {
+    // return an array of objects located in a specific city.
+    const filterStudents = listOfStudents.filter(student => student.location === city);
+    return filterStudents;
+}

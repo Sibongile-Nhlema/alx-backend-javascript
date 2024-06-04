be72b471391b6314bddb2cb2a7b0e3ef6a64f620
Sibongile <sibongile.nhlema@gmail.com>
2024-06-04 11:29:42 +0200

    function returns an ArrayBuffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,15 @@
-export default function createInt8TypedArray(length, position, value) {
-    // returns a new ArrayBuffer with an Int8 value at a specific position.
-    
-}
+const createInt8TypedArray = (length, position, value) => {
+  // returns a new ArrayBuffer with an Int8 value at a specific position.
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+
+  return view;
+};
+
+export default createInt8TypedArray;

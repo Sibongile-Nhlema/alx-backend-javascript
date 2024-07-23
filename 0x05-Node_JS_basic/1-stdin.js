1f115b09782302752053b013a682e1d7a9cfefb6
Sibongile <sibongile.nhlema@gmail.com>
2024-07-23 19:42:05 +0200

    removed one line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,6 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.exit();
 });
 
 // exit message

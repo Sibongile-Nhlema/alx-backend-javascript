cc443a152852fefec455edac9156df1531df0fc3
Sibongile <sibongile.nhlema@gmail.com>
2024-07-23 21:52:19 +0200

    pushing mock code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,10 @@
+function countStudents(path) {
+    try {
+        console.log("Test run")
+    }
+    catch (err){
+        throw new Error('Cannot load the database')
+    }
+}
 
+module.exports = countStudents;

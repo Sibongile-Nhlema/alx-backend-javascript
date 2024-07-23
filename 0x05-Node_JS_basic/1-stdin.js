e74c8b0a38bdd89dc16f27f205d86b588b28f944
Sibongile <sibongile.nhlema@gmail.com>
2024-07-23 19:16:37 +0200

    fixedd eslint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,32 +2,32 @@ const readline = require('readline');
 
 // Set up the interface to handle interactive input
 const readl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Flag to determine if the exit message should be displayed
 let shouldDisplayExitMessage = true;
 
 // Handle interactive user input
 readl.on('line', (input) => {
-    const name = input.trim();
-    console.log(`Your name is: ${name}`);
-    readl.close();
-    if (shouldDisplayExitMessage) {
-        console.log("This important software is now closing");
-    }
+  const name = input.trim();
+  console.log(`Your name is: ${name}`);
+  readl.close();
+  if (shouldDisplayExitMessage) {
+    console.log('This important software is now closing');
+  }
 });
 
 // Check if the process is receiving piped input, if not don't print
 process.stdin.on('data', () => {
-    shouldDisplayExitMessage = false;
+  shouldDisplayExitMessage = false;
 });
 
 process.stdin.on('end', () => {
-    if (shouldDisplayExitMessage) {
-        console.log("This important software is now closing");
-    }
+  if (shouldDisplayExitMessage) {
+    console.log('This important software is now closing');
+  }
 });

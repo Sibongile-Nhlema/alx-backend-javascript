740f3e38f62c6115e521ec6ba226ae5cfdb167b1
Sibongile <sibongile.nhlema@gmail.com>
2024-07-23 19:03:03 +0200

    rewrote the program

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,33 @@
-// display message
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+const readline = require('readline');
 
-// user inputs
-process.stdin.on("data", (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-    process.exit();
+// Set up the interface to handle interactive input
+const readl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-// exit message
-process.on("exit", () => {
-    console.log("This important software is now closing");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+// Flag to determine if the exit message should be displayed
+let shouldDisplayExitMessage = true;
+
+// Handle interactive user input
+readl.on('line', (input) => {
+    const name = input.trim();
+    console.log(`Your name is: ${name}`);
+    readl.close();
+    if (shouldDisplayExitMessage) {
+        console.log("This important software is now closing");
+    }
+});
+
+// Check if the process is receiving piped input, if not don't print
+process.stdin.on('data', () => {
+    shouldDisplayExitMessage = false;
+});
+
+process.stdin.on('end', () => {
+    if (shouldDisplayExitMessage) {
+        console.log("This important software is now closing");
+    }
 });

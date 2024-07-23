b6329a171e01274cddb53c6f5594883c05198891
Sibongile <sibongile.nhlema@gmail.com>
2024-07-23 22:11:05 +0200

    removed the newline

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   // Set the response header
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245, () => {

5e2bb70b053b48d29ee8da43446b8e473d3f3793
Sibongile <sibongile.nhlema@gmail.com>
2024-07-23 21:18:04 +0200

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    // Try to read the file asynchronously
+    const data = await fs.readFile(path, 'utf8');
+
+    // Split the content by lines and filter out empty ones
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    const fieldCounts = {};
+    const fieldLists = {};
+
+    // Process every line after the header
+    for (let i = 1; i < lines.length; i++) {
+      const [firstname, , , field] = lines[i].split(',');
+      if (firstname && field) {
+        if (!fieldCounts[field]) {
+          fieldCounts[field] = 0;
+          fieldLists[field] = [];
+        }
+        fieldCounts[field]++;
+        fieldLists[field].push(firstname);
+      }
+    }
+
+    // Log total number of students
+    const totalStudents = lines.length - 1;
+    console.log(`Number of students: ${totalStudents}`);
+
+    // Log details for each field
+    for (const [field, count] of Object.entries(fieldCounts)) {
+      const list = fieldLists[field].join(', ');
+      console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;

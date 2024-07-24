fefc3783a7ddabe1a8930050e7f46aae45650e02
Sibongile <sibongile.nhlema@gmail.com>
2024-07-24 19:23:25 +0200

    added the exit child process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,6 +5,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+  process.exit();
 });
 
 // exit message
